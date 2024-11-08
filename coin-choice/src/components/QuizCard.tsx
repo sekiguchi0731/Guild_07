cee575b0ba9255e54c0cfcbf56444668a3c89c5c
Yuya757 <pantera.yk37@gmail.com>
2024-11-09 07:08:37 +0900

    wip; カード部分改善

--- coin-choice/src/components/QuizCard.tsx
+++ coin-choice/src/components/QuizCard.tsx
@@ -1,21 +1,79 @@
 // src/components/QuizCard.tsx
 import React from 'react';
-import './QuizCard.scss'; // スタイルシートをインポート
 
-interface QuizCardProps {
-  question: string;
-  children: React.ReactNode;
-}
-
-const QuizCard: React.FC<QuizCardProps> = ({ question, children }) => {
+const QuizCard: React.FC<{ question: string; children: React.ReactNode }> = ({
+  question,
+  children,
+}) => {
   return (
     <div
       className='quiz-card'
-      style={{ backgroundImage: `url(/assets/images/card-background.svg)` }}
+      style={{
+        borderRadius: '40px',
+        width: '300px',
+        height: '70vh',
+        margin: window.innerWidth >= 450 ? '0 auto' : 'auto',
+        boxShadow: '0 4px 12px rgba(0, 0, 0, 0.2)',
+        backgroundImage: 'url(../../assets/images/quiz/quizBackground.webp)',
+        position: 'relative',
+      }}
     >
-      <div className='quiz-container'>
-        <h2 className='quiz-question'>{question}</h2>
-        <div className='quiz-content'>{children}</div>
+      <img
+        src='../../assets/images/quiz/ribbon.webp'
+        alt='Overlay'
+        style={{
+          width: '100%',
+          position: 'relative',
+          top: '10%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          zIndex: 1,
+        }}
+      />
+      <img
+        src='../../assets/images/quiz/blossom.webp'
+        alt='Overlay'
+        style={{
+          width: '30%',
+          position: 'absolute',
+          top: '10%',
+          left: '85%',
+          transform: 'translate(-50%, -50%)',
+          zIndex: 1,
+        }}
+      />
+      <div
+        className='quiz-container'
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          marginTop: '30vh',
+          gap: '20px',
+        }}
+      >
+        <h2
+          className='quiz-question'
+          style={{
+            fontSize: '18px',
+            marginBottom: '20px',
+            color: '#333333',
+          }}
+        >
+          {question}
+        </h2>
+        <div
+          className='quiz-content'
+          style={{
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '50px',
+          }}
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
